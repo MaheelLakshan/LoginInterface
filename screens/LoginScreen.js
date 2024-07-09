cf31876072cd1795ba8bdcaf956c21781fa97e77
MaheelLakshan <MaheelLakshanra@gmail.com>
2024-07-10 00:30:42 +0530

    login page completed

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,10 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Fontisto from 'react-native-vector-icons/Fontisto';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import { TextInput } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const LoginScreen = () => {
   return (
@@ -11,7 +13,11 @@ const LoginScreen = () => {
         <Image source={require('../assets/topVector.png')} style={styles.topImage} />
       </View>
       <View style={styles.helloContainer}>
-        <Text style={styles.helloText}>Hello</Text>
+        <Text style={styles.helloText}>
+          <Text style={{ color: '#F97794', fontWeight: 'bold' }}>A</Text>
+          nomaly <Text style={{ color: '#623AA2', fontWeight: 'bold' }}>D</Text>
+          etection
+        </Text>
       </View>
       <View>
         <Text style={styles.signInText}>Sign in to your account</Text>
@@ -26,6 +32,20 @@ const LoginScreen = () => {
         <TextInput style={styles.textInput} placeholder="Password" secureTextEntry />
       </View>
       <Text style={styles.forgotPassword}>Forgot your password</Text>
+
+      <View style={styles.signInButtonContainer}>
+        <Text style={styles.signIn}>Sign In</Text>
+        <LinearGradient colors={['#F97794', '#623AA2']} style={styles.linearGradient}>
+          <AntDesign style={styles.inputIcon} name="arrowright" size={20} color={'white'} />
+        </LinearGradient>
+      </View>
+      <Text style={styles.footerText}>
+        Don't have an account? <Text style={{ textDecorationLine: 'underline' }}>Create</Text>
+      </Text>
+
+      <View style={styles.leftVectorContainer}>
+        <ImageBackground source={require('../assets/leftVector.png')} style={styles.leftVectorImage} />
+      </View>
     </View>
   );
 };
@@ -36,6 +56,7 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#F5F5F5',
     flex: 1,
+    position: 'relative',
   },
   topImageContainer: {},
   topImage: {
@@ -72,11 +93,44 @@ const styles = StyleSheet.create({
   inputIcon: {
     marginLeft: 15,
   },
-
   forgotPassword: {
     color: '#BEBEBE',
     textAlign: 'right',
     width: '90%',
     fontSize: 15,
+  },
+  signInButtonContainer: {
+    flexDirection: 'row',
+    marginTop: 100,
+    width: '90%',
+    justifyContent: 'flex-end',
+  },
+  signIn: {
+    color: '#262626',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  linearGradient: {
+    height: 34,
+    width: 56,
+    borderRadius: 17,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 10,
+  },
+  footerText: {
+    color: '#262626',
+    textAlign: 'center',
+    fontSize: 18,
+    marginTop: 20,
+  },
+  leftVectorContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+  },
+  leftVectorImage: {
+    height: 250,
+    width: 200,
   },
 });
